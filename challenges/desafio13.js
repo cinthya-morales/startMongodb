670f626bc0a2ca98d1c8acfb88fda2c7e438ff9f
Cinthya Morales <cinthya.morales@outlook.com>
2022-09-12 19:01:29 -0300

    fix: Consertando o requisito 13

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,2 +1,2 @@
-dd.produtos.updateMany({ $set: { criadoPor: "Ronald McDonald" } });
+db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } });
 db.produtos.find({}, { _id: 0, nome: 1, criadoPor: 1 });
