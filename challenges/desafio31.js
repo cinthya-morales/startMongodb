00437e4cb16c53e1b4478faa0f4b7e9b31261f0a
Cinthya Morales <cinthya.morales@outlook.com>
2022-09-13 18:22:07 -0300

    feat: Requisito 31

---
+++ challenges/desafio31.js
@@ -0,0 +1,2 @@
+// Referência pra resposta https://stackoverflow.com/questions/4442453/mongodb-query-condition-on-comparing-2-fields
+db.produtos.find({ $expr: { $gt: ["$curtidas", "$vendidos"] } }, { _id: 0, nome: 1 });
